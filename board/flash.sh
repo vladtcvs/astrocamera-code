c4f420ace4ac03d2db522ecb1f6610f8fe0f4188
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2025-08-19 01:20:45 +0200

    Add script for boundary scan

--- board/flash.sh
+++ board/flash.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
-BINARY="../build/src/application/camera-control-code.hex"
+
+CURFILE=$(realpath "$0")
+CURPATH=$(dirname "$CURFILE")
+DEFAULT_BINARY="$CURPATH/../build/src/application/camera-control-code.hex"
+BINARY="${1:-$DEFAULT_BINARY}"
+
+BINARY=$(realpath "$BINARY")
+echo "Using binary: $BINARY"
+echo
+echo
 
 openocd -f interface/jlink.cfg -c 'transport select jtag; adapter speed 400'  -f board.cfg -c "program $BINARY verify reset exit"
