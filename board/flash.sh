1443ac6b8df994fc7fd32bc68c153428f4a5f6e6
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2025-08-18 13:04:05 +0200

    Fix board and flash

--- board/flash.sh
+++ board/flash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BINARY="../build/src/application/camera-control-code.hex"
 
-openocd -f interface/jlink.cfg -f jtag.cfg -c "program $BINARY verify reset exit"
+openocd -f interface/jlink.cfg -f board.cfg -c "program $BINARY verify reset exit"
