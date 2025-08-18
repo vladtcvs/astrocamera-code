c8201f663a122c9dfd289a206273c92b5145eae5
Vladislav Tsendrovskii <vtcendrovskii@gmail.com>
2025-08-18 13:06:25 +0200

    Update flash script

--- board/flash.sh
+++ board/flash.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 BINARY="../build/src/application/camera-control-code.hex"
 
-openocd -f interface/jlink.cfg -f board.cfg -c "program $BINARY verify reset exit"
+openocd -f interface/jlink.cfg -c 'transport select jtag; adapter speed 400'  -f board.cfg -c "program $BINARY verify reset exit"
--- board/run-ocd.sh
+++ board/run-ocd.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-openocd -f interface/jlink.cfg -f board.cfg
+openocd -f interface/jlink.cfg -c 'transport select jtag; adapter speed 400' -f board.cfg
 
